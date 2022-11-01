5b674d9ad2c1e1d5b6c7cfbf3de6235d1442f351
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-11-01 22:42:19 +0100

    hotel reservation

--- src/HotelApplication.java
+++
@@ -1,5 +0,0 @@
-public class HotelApplication {
-    public static void main(String[] args) {
-        MainMenu.menu();
-    }
-}
--- src/model/IRoom.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public interface IRoom {
-    String getRoomNumber();
-    Double getRoomPrice();
-    RoomType getRoomType();
-    boolean isFree();
-}
